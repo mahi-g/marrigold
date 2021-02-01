37404e600133852219188411b2af1c161a9ba52c
tahiya-mongo <tahiya.chowdhury@10gen.com>
2021-01-31 20:07:12 -0500

    Add form card component

---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormCard from '../components/card/FormCard';
+
+const Test = () => (
+    <FormCard
+        header='Header'
+        height={300}
+        width={500}
+    />
+);
+
+export default Test;
