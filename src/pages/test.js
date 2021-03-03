cd89680bd08abf9f3eca14f2fdd97ed386425a04
mahi-g <gm1585@nyu.edu>
2021-03-02 21:41:58 -0500

    Fix merge error by restoring older files

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Review from './review';
+import CategoryCard from '../components/card/CategoryCard';
+import photographer from "../images/photographer.png";
+import styled from '@emotion/styled';
+
+const StyledCategoryCard = styled(CategoryCard)`
+    margin-left: 100px;
+`;
 
 const Test = () => (
-    //Testing multip step form / signup component
-    <>
-        <Review/>
-    </>
+    //Testing the category card
+
+    <StyledCategoryCard
+        categoryName={'Photographer'}
+        image={photographer}
+        href={'/photographer'}
+    />
 );
-
-
 
 export default Test;
